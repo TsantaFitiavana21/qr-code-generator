ba230f635b191dc8ebc52e20ca8c8085de4cd44e
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2024-01-22 00:24:49 +0300

    feat: adding download

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-function generateQRCode() {
+function generateQRCode(color) {
     let website = document.getElementById("website").value
     if (website) {
         let qrcodeContainer = document.getElementById("qrcode")
         qrcodeContainer.innerHTML = ""
-        new QRCode(qrcodeContainer, website)
-        
-        /*With some styles*/
-        // let qrcodeContainer2 = document.getElementById("qrcode-2")
-        // qrcodeContainer2.innerHTML = ""
-        // new QRCode(qrcodeContainer2, {
-        //     text: website,
-        //     width: 128,
-        //     height: 128,
-        //     colorDark: "#5868bf",
-        //     colorLight: "#ffffff",
-        //     correctLevel: QRCode.CorrectLevel.H,
-        // })
-        // document.getElementById("qrcode-container").style.display = "block"
+        new QRCode(qrcodeContainer, {
+            text: website,
+            colorDark: typeof color == "string" ? color : "#1a1a1a",
+        })
     } else {
         alert("Please enter a valid URL")
     }
+}
+
+function getColor(element) {
+    var bgColor = window.getComputedStyle(element).backgroundColor
+    var hexColor = rgbToHex(bgColor)
+    generateQRCode(hexColor)
+}
+
+function rgbToHex(rgb) {
+    var rgbArray = rgb.match(/\d+/g)
+    var hex =
+        "#" +
+        ("0" + parseInt(rgbArray[0], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgbArray[1], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgbArray[2], 10).toString(16)).slice(-2)
+    return hex.toUpperCase()
+}
+
+function downloadQRCode() {
+    // Get the QR code image element
+    var qrCodeImage = document.getElementById("qrcode")
+    var image = qrCodeImage.querySelector("img")
+    console.log(image)
+
+    // Create a download link
+    var downloadLink = document.createElement("a")
+    downloadLink.href = image.src
+    downloadLink.download = "qr_code.png"
+
+    // Trigger the download
+    downloadLink.click()
 }
 
 window.onload = generateQRCode
